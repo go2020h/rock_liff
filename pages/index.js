198e797822e05d18d88d21dddb86554bd4a82c9d
go2020h <go2020.hidane.inc@gmail.com>
2024-09-25 23:21:09 +0900

    update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home({ liff, liffError }) {
+  const [profile, setProfile] = useState(null)
+
+  useEffect(() => {
+    if (liff && liff.isLoggedIn()) {
+      liff
+        .getProfile()
+        .then((profileData) => {
+          setProfile(profileData)
+          console.log('Profile:', profileData)
+        })
+        .catch((err) => {
+          console.log('error', err)
+        })
+    }
+  }, [liff])
+
   return (
     <div>
       <Head>
@@ -20,6 +37,16 @@ export default function Home({ liff, liffError }) {
               <code>{liffError}</code>
             </p>
           </>
+        )}
+        {profile && (
+          <div>
+            <h2>User Profile</h2>
+            <p>Name: {profile.displayName}</p>
+            <p>User ID: {profile.userId}</p>
+            {profile.pictureUrl && (
+              <img src={profile.pictureUrl} alt="Profile" style={{ width: '100px', height: '100px' }} />
+            )}
+          </div>
         )}
         <a href="https://developers.line.biz/ja/docs/liff/" target="_blank" rel="noreferrer">
           LIFF Documentation
